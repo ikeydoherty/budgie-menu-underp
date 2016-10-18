f83be10266eb9bfb72fb4b4447c2fe3c879d6098
Ikey Doherty <ikey@solus-project.com>
2016-10-18 23:24:59 +0100

    Fix linking order for -Wl,-as-needed (somehow forced in Ubuntu..?)
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- mksolus.sh
+++ mksolus.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-gcc -Wall `pkg-config --cflags --libs libgnome-menu-3.0` `pkg-config --cflags --libs gtk+-3.0` -g3 -DGMENU_I_KNOW_THIS_IS_UNSTABLE solus/*.c -o SolusTest
+gcc -Wall -g3 -DGMENU_I_KNOW_THIS_IS_UNSTABLE solus/*.c `pkg-config --cflags --libs libgnome-menu-3.0` `pkg-config --cflags --libs gtk+-3.0`  -o SolusTest
