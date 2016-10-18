af2adeb7c8a018859b8bd3fc855298685dddfab2
Ikey Doherty <ikey@solus-project.com>
2016-10-18 22:10:11 +0100

    Unbreak define
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-valac -X -D-DGMENU_I_KNOW_THIS_IS_UNSTABLE --pkg gtk+-3.0  --pkg libgnome-menu-3.0 --pkg gio-unix-2.0 *.vala -o CruftTest
+valac -X -DGMENU_I_KNOW_THIS_IS_UNSTABLE --pkg gtk+-3.0  --pkg libgnome-menu-3.0 --pkg gio-unix-2.0 *.vala -o CruftTest
 
 
