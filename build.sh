2c361e9d56a5d7225f2359dcc4b8e04cda607a30
Ikey Doherty <ikey@solus-project.com>
2016-10-18 22:13:23 +0100

    Modify build script for debug purposes
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-valac -X -DGMENU_I_KNOW_THIS_IS_UNSTABLE --pkg gtk+-3.0  --pkg libgnome-menu-3.0 --pkg gio-unix-2.0 *.vala -o CruftTest
+valac --save-temps -g --enable-checking -X -DGMENU_I_KNOW_THIS_IS_UNSTABLE --pkg gtk+-3.0  --pkg libgnome-menu-3.0 --pkg gio-unix-2.0 *.vala -o CruftTest
 
 
